cab73db4ce6efe5000e203548087791b56d38e31
Sheila <sheilanievalo.2000@gmail.com>
2022-05-30 20:32:34 +0200

    Botones redondeados y diseño principal

--- SheiStyleSNL/FormPrincipal.cs
+++ SheiStyleSNL/FormPrincipal.cs
@@ -10,11 +10,17 @@ using System.Windows.Forms;
 
 namespace SheiStyleSNL
 {
-    public partial class Form1 : Form
+    public partial class FormPrincipal : Form
     {
-        public Form1()
+        public FormPrincipal()
         {
             InitializeComponent();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
