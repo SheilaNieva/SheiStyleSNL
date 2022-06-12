67dc9a1dbfc7f8729249b5ab85486114d524a3e4
Sheila <sheilanievalo.2000@gmail.com>
2022-06-12 21:03:32 +0200

    SplashScreen

--- SheiStyleSNL/Program.cs
+++ SheiStyleSNL/Program.cs
@@ -16,7 +16,7 @@ namespace SheiStyleSNL
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormPrincipal());
+            Application.Run(new FormSplashScreen());
         }
     }
 }
